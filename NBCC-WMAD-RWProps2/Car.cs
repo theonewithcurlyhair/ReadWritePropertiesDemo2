7f992b0afff3d833f71eb8b0a95b686fbe6d96da
Nikita Mieshalnykov <48445041+theonewithcurlyhair@users.noreply.github.com>
2019-05-03 10:39:35 -0300

    CarBuilder

---
+++ NBCC-WMAD-RWProps2/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBCC_WMAD_Console
+{
+    public class Car
+    {
+        private int? speed;
+        private Nullable<decimal> price;
+        private int numberOfDoors;
+
+
+        public int? Year { get; set; }
+
+        public decimal? Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public int? Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public string CarType { get; set; }
+
+        public int NumberOfDooers
+        {
+            get
+            {
+                return numberOfDoors;
+            }
+            set
+            {
+                if (value < 10)
+                {
+                    numberOfDoors = value;
+                }
+            }
+        }
+
+
+    }
+}
--- NBCC-WMAD-RWProps2/Program.cs
+++ NBCC-WMAD-RWProps2/Program.cs
@@ -15,7 +15,8 @@ namespace NBCC_WMAD_Console
         private static void LoadMenu()
         {
             Menu.menuDictionary.Add(0, "Exit");
-            Menu.menuDictionary.Add(1, "About the Console");            
+            Menu.menuDictionary.Add(1, "About the Console");
+            Menu.menuDictionary.Add(2, "Car Builder");
         }
 
         static void Main(string[] args)
@@ -33,13 +34,54 @@ namespace NBCC_WMAD_Console
                         AboutThisApp();
                         break;
                     //Add more options in the menu here
+                    case 2:
+                        CarBuilder();
+                        break;
                 }
             }
 
             Out.P("Press any key to exit.");
             Console.ReadLine();
         }
-                
+
+        static List<Car> carList = new List<Car>();
+
+        private static void CarBuilder()
+        {
+            string userCarType = In.GetString("Provide a car type (car, turck van)");
+            int userNumberOfDoors = In.GetInt("Provide the number of doors: ");
+            int userCarSpeed = In.GetInt("Provide car speed: ");
+
+
+            Car car = new Car();
+
+            car.CarType = userCarType;
+            car.NumberOfDooers = userNumberOfDoors;
+            car.Speed = userCarSpeed;
+
+
+            Out.P("************************");
+
+            Out.P($"The car created is: \n {car.CarType} \n Number of doors {car.NumberOfDooers} \n Car speed: {car.Speed} \n Car price: {car.Price.GetValueOrDefault()}");
+
+            Out.P("************************ \n");
+
+
+            carList.Add(car);
+
+            Out.P($"{carList.Count} cars in my garage");
+
+            string userShowCars = In.GetString("Do you want to see list of the cars (y/n)");
+            if (userShowCars == "y")
+            {
+                foreach(Car cr in carList)
+                {
+                    Out.P($"\n Car Type: {cr.CarType} \n Number of doors: {cr.NumberOfDooers} \n Car Speed {cr.Speed}");
+                }
+            }
+
+        }
+
         #region [My Functionality]
 
         /*
@@ -57,7 +99,7 @@ namespace NBCC_WMAD_Console
             Out.P("This app is the Console Root Application. Build on it by providing new menu items and adding to the switch statement");
             Out.P("\n");
         }
-        
+
         /// <summary>
         /// Initialize the Console
         /// </summary>
